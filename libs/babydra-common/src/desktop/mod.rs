d149759638ad3d63b39e607c2787ea634b006a37
tdkhoa <I4104.vn@gmail.com>
2026-07-08 09:16:40 +0700

    fix(desktop): re-export get_window_hash under desktop module

--- libs/babydra-common/src/desktop/mod.rs
+++ libs/babydra-common/src/desktop/mod.rs
@@ -8,5 +8,5 @@ pub mod window;
 pub mod apps;
 pub mod tray;
 
-pub use apps::{find_desktop_apps, refresh_desktop_apps_cache, DesktopApp};
+pub use apps::{find_desktop_apps, refresh_desktop_apps_cache, DesktopApp, get_window_hash};
 
