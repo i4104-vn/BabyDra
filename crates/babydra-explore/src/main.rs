036304672c306ed49a396fe98b984ea8a083b324
tdkhoa <i4104.vn@gmail.com>
2026-07-10 14:50:35 +0700

    fix(explore): initialize tokio runtime context for dbus spawning

--- crates/babydra-explore/src/main.rs
+++ crates/babydra-explore/src/main.rs
@@ -4,6 +4,9 @@ mod ui;
 use app::BabyExploreApp;
 
 fn main() {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let _guard = rt.enter();
+
     let app = BabyExploreApp::new();
     let exit_code = app.run();
     std::process::exit(exit_code);
