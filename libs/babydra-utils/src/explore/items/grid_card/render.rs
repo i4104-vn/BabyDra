8175ff88595cf6dd4885ed2b03f85bc9e349a8fa
tdkhoa <i4104.vn@gmail.com>
2026-07-23 14:35:52 +0700

    style(explore): make grid item cards uniform 108x108 square size

--- libs/babydra-utils/src/explore/items/grid_card/render.rs
+++ libs/babydra-utils/src/explore/items/grid_card/render.rs
@@ -5,10 +5,19 @@ use babydra_common::{FileEntry, load_cropped_square_pixbuf};
 /// Builds the visual GTK layout box for a grid item card.
 pub fn build_grid_card_ui(entry: &FileEntry) -> Box {
     let item_box = Box::new(Orientation::Vertical, 4);
-    item_box.set_size_request(100, 110);
+    item_box.set_size_request(108, 108);
     item_box.set_css_classes(&["file-item"]);
-    item_box.set_halign(Align::Fill);
-    item_box.set_valign(Align::Fill);
+    item_box.set_halign(Align::Center);
+    item_box.set_valign(Align::Center);
+    item_box.set_hexpand(false);
+    item_box.set_vexpand(false);
+
+    let icon_frame = Box::new(Orientation::Vertical, 0);
+    icon_frame.set_size_request(64, 64);
+    icon_frame.set_halign(Align::Center);
+    icon_frame.set_valign(Align::Center);
+    icon_frame.set_hexpand(false);
+    icon_frame.set_vexpand(false);
 
     let has_preview = if let Some(ext) = entry.path.extension() {
         let ext_str = ext.to_string_lossy().to_lowercase();
@@ -19,16 +28,17 @@ pub fn build_grid_card_ui(entry: &FileEntry) -> Box {
 
     if has_preview {
         let overlay = gtk4::Overlay::new();
+        overlay.set_size_request(64, 64);
+        overlay.set_halign(Align::Center);
+        overlay.set_valign(Align::Center);
         
         let temp_icon = crate::ui::icon::get_system_or_file_icon(&entry.icon_name, "text-x-generic");
-        temp_icon.set_pixel_size(52);
+        temp_icon.set_pixel_size(48);
         temp_icon.set_halign(Align::Center);
         temp_icon.set_valign(Align::Center);
-        temp_icon.set_hexpand(true);
-        temp_icon.set_vexpand(true);
         
         overlay.set_child(Some(&temp_icon));
-        item_box.append(&overlay);
+        icon_frame.append(&overlay);
 
         struct SendWrapper<T>(T);
         unsafe impl<T> Send for SendWrapper<T> {}
@@ -38,37 +48,36 @@ pub fn build_grid_card_ui(entry: &FileEntry) -> Box {
         
         glib::spawn_future_local(async move {
             let res = tokio::task::spawn_blocking(move || {
-                load_cropped_square_pixbuf(&path_clone, 85).map(|pb| SendWrapper(pb))
+                load_cropped_square_pixbuf(&path_clone, 64).map(|pb| SendWrapper(pb))
             }).await;
 
             if let Ok(Ok(wrapper)) = res {
                 let pixbuf = wrapper.0;
                 let texture = gtk4::gdk::Texture::for_pixbuf(&pixbuf);
                 let picture = Picture::for_paintable(&texture);
-                picture.set_size_request(85, 85);
+                picture.set_size_request(64, 64);
                 picture.set_halign(Align::Center);
                 picture.set_valign(Align::Center);
-                picture.set_hexpand(true);
-                picture.set_vexpand(true);
                 picture.set_content_fit(gtk4::ContentFit::Cover);
                 overlay_c.set_child(Some(&picture));
             }
         });
     } else {
         let icon = crate::ui::icon::get_system_or_file_icon(&entry.icon_name, "text-x-generic");
-        icon.set_pixel_size(52);
+        icon.set_pixel_size(48);
         icon.set_halign(Align::Center);
         icon.set_valign(Align::Center);
-        icon.set_hexpand(true);
-        icon.set_vexpand(true);
-        item_box.append(&icon);
+        icon_frame.append(&icon);
     };
+
+    item_box.append(&icon_frame);
 
     let lbl = Label::builder()
         .label(&entry.display_name)
-        .max_width_chars(12)
+        .max_width_chars(11)
         .ellipsize(gtk4::pango::EllipsizeMode::End)
         .halign(Align::Center)
+        .valign(Align::Center)
         .hexpand(true)
         .build();
     lbl.add_css_class("file-item-label");
